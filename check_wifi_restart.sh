b8a7dd974977337303d3048364c504d476070aa4
bjornstromberg <5454791+bjornstromberg@users.noreply.github.com>
2020-04-18 14:49:28 +0200

    added initial restart script

---
+++ check_wifi_restart.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# list the interfaces names.
+INTERFACES=$(iw dev | awk '$1=="Interface"{print $2}')
+
+# list interfaces that does not have any clients connected.
+for INTERFACE in $INTERFACES
+do
+  ASSOC_CLIENTS=`iw dev $INTERFACE station dump | awk '$1=="Station"' | grep -c "Station"`
+
+  if [ "$ASSOC_CLIENTS" -lt 1 ]
+  then
+    RESTART_LIST="$RESTART_LIST $INTERFACE"
+  fi
+done
+
+if [ ! -z $RESTART_LIST ]
+then
+  echo "$(date +"%F_%T"): restarting wifi - issues with $RESTART_LIST"
+  wifi down
+  sleep 1
+  wifi up
+fi
